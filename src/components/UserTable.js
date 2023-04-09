857b0b822c7d7060a1fa233fe4f0c287988b8b8b
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-09 10:44:51 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,6 +1,46 @@
+import React, { useEffect, useState } from "react";
 
-import React, { useState, useEffect } from "react";
+function UserTable() {
+  const [users, setUsers] = useState([]);
 
-function UserTable() {}
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      setUsers(data.sort((a, b) => a.id - b.id));
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default UserTable;
+// import React, { useState, useEffect } from "react";
+
+// function UserTable() {}
+
+// export default UserTable;
