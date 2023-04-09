eeb1b5d9a37bcdef570be7f281f84b9343937d63
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-09 10:06:10 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,6 +1,6 @@
 
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// function UserTable() {}
+function UserTable() {}
 
-// export default UserTable;
+export default UserTable;
